abe42655bad0a579a2147064970ce28270c62670
Dylan Oldham <114247239+DylanOldham@users.noreply.github.com>
2023-02-22 12:17:02 -0800

    Added controls help

--- client/index.js
+++ client/index.js
@@ -11,5 +11,19 @@ window.addEventListener("resize", (event) => {
     canvas.height = canvas.clientHeight;
 })
 
+let helpButton = document.querySelector("#help-button");
+let helpDropdown = document.querySelector("#help-dropdown");
+helpButton.addEventListener("click", (event) => {
+    if (!helpButton.open) {
+        helpButton.open = true;
+        helpButton.textContent = "Close Controls";
+        helpDropdown.style = "position: relative; visibility: visible;";
+    } else {
+        helpButton.open = false;
+        helpButton.textContent = "View Controls";
+        helpDropdown.style = "position: fixed; visibility: hidden;";
+    }
+});
+
 let game = new Game(canvas);
 game.Run();
